d7ef6b021b12b41e9fd82fcf286a108841f20359
YukiWatanabe824 <gyggg950@gmail.com>
2022-12-17 06:33:40 +0900

    refactor: hasownpropertyの記述をESLintに応じてを修正 ”

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,26 +1,25 @@
 const moment = require("moment");
 const argv = require("minimist")(process.argv.slice(2));
 
-let year = argv.hasOwnProperty("y") ? argv.y : moment().year();
-let month = argv.hasOwnProperty("m") ? argv.m - 1 : moment().month();
-
+let year = Object.prototype.hasOwnProperty.call(argv, "y")
+  ? argv.y
+  : moment().year();
+let month = Object.prototype.hasOwnProperty.call(argv, "m")
+  ? argv.m - 1
+  : moment().month();
 let date = moment([year, month, 1]);
 
 let last_date = date.endOf("month");
-
 const LAST_DAY = last_date.date();
 
 let blanks = date.startOf("month").weekday();
 let full_days = new Array();
-
 for (let i = 1; i <= blanks; i++) {
   full_days.push("  ");
 }
-
 for (let i = 1; i <= LAST_DAY; i++) {
   full_days.push(i);
 }
-
 full_days = full_days.map(function (element) {
   return element.toString().padStart(2, " ");
 });
@@ -34,4 +33,4 @@ for (let i = 0; i < full_days.length; i++) {
     process.stdout.write(full_days[i] + " ");
   }
 }
-console.log('')
+console.log("");
