9cd1c4a7672c089296ad32e9c0e083fb7bb0d499
YukiWatanabe824 <gyggg950@gmail.com>
2022-12-17 06:21:28 +0900

    feat: 指定日程での表示に対応

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,33 +1,37 @@
-const moment = require('moment');
-let dt = moment();
-let first_date = moment([dt.year(), dt.month(), 1])
-let last_date = dt.endOf('month')
+const moment = require("moment");
+const argv = require("minimist")(process.argv.slice(2));
 
-const LAST_DAY = last_date.date()
+let year = argv.hasOwnProperty("y") ? argv.y : moment().year();
+let month = argv.hasOwnProperty("m") ? argv.m - 1 : moment().month();
 
-let blanks = first_date.weekday()
-debugger
-let full_days = new Array()
+let date = moment([year, month, 1]);
 
-for(let i = 1; i <= blanks; i++){
-  full_days.push('  ')
+let last_date = date.endOf("month");
+
+const LAST_DAY = last_date.date();
+
+let blanks = date.startOf("month").weekday();
+let full_days = new Array();
+
+for (let i = 1; i <= blanks; i++) {
+  full_days.push("  ");
 }
 
-for(let i = 1; i <= LAST_DAY; i++){
-  full_days.push(i)
+for (let i = 1; i <= LAST_DAY; i++) {
+  full_days.push(i);
 }
 
-full_days = full_days.map(function(element){
-  return element.toString().padStart(2, ' ')
-})
+full_days = full_days.map(function (element) {
+  return element.toString().padStart(2, " ");
+});
 
-
-console.log(`      ${dt.month()+1}月 ${dt.year()}`)
-console.log("日 月 火 水 木 金 土")
-for(let i = 0; i < full_days.length; i++){
-  if ((i + 1) % 7 === 0){
-    process.stdout.write(full_days[i] + '\n')
-  }else{
-    process.stdout.write(full_days[i] + ' ')
+console.log(`      ${date.month() + 1}月 ${date.year()}`);
+console.log("日 月 火 水 木 金 土");
+for (let i = 0; i < full_days.length; i++) {
+  if ((i + 1) % 7 === 0) {
+    process.stdout.write(full_days[i] + "\n");
+  } else {
+    process.stdout.write(full_days[i] + " ");
   }
 }
+console.log('')
