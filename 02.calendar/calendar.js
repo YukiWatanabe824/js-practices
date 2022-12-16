3acea2f360b6baef6542651a71ff51150a962782
YukiWatanabe824 <gyggg950@gmail.com>
2022-12-17 06:51:35 +0900

    refactor: 処理を関数化し、見通しよく

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,36 +1,49 @@
 const moment = require("moment");
 const argv = require("minimist")(process.argv.slice(2));
 
-let year = Object.prototype.hasOwnProperty.call(argv, "y")
-  ? argv.y
-  : moment().year();
-let month = Object.prototype.hasOwnProperty.call(argv, "m")
-  ? argv.m - 1
-  : moment().month();
-let date = moment([year, month, 1]);
-
-let last_date = date.endOf("month");
-const LAST_DAY = last_date.date();
-
-let blanks = date.startOf("month").weekday();
-let full_days = new Array();
-for (let i = 1; i <= blanks; i++) {
-  full_days.push("  ");
+function get_date(argv) {
+  let year = Object.prototype.hasOwnProperty.call(argv, "y")
+    ? argv.y
+    : moment().year();
+  let month = Object.prototype.hasOwnProperty.call(argv, "m")
+    ? argv.m - 1
+    : moment().month();
+  return moment([year, month, 1]);
 }
-for (let i = 1; i <= LAST_DAY; i++) {
-  full_days.push(i);
-}
-full_days = full_days.map(function (element) {
-  return element.toString().padStart(2, " ");
-});
 
-console.log(`      ${date.month() + 1}月 ${date.year()}`);
-console.log("日 月 火 水 木 金 土");
-for (let i = 0; i < full_days.length; i++) {
-  if ((i + 1) % 7 === 0) {
-    process.stdout.write(full_days[i] + "\n");
-  } else {
-    process.stdout.write(full_days[i] + " ");
+function generate_full_days(date) {
+  let last_date = date.endOf("month");
+  const LAST_DAY = last_date.date();
+
+  let blanks = date.startOf("month").weekday();
+  let full_days = new Array();
+  for (let i = 1; i <= blanks; i++) {
+    full_days.push("  ");
   }
+
+  for (let i = 1; i <= LAST_DAY; i++) {
+    full_days.push(i);
+  }
+
+  full_days = full_days.map(function (element) {
+    return element.toString().padStart(2, " ");
+  });
+  return full_days;
 }
-console.log("");
+
+function generate_calendar(date, full_days) {
+  console.log(`      ${date.month() + 1}月 ${date.year()}`);
+  console.log("日 月 火 水 木 金 土");
+  for (let i = 0; i < full_days.length; i++) {
+    if ((i + 1) % 7 === 0) {
+      process.stdout.write(full_days[i] + "\n");
+    } else {
+      process.stdout.write(full_days[i] + " ");
+    }
+  }
+  console.log("");
+}
+
+let date = get_date(argv);
+let full_days = generate_full_days(date);
+generate_calendar(date, full_days);
