811f6a688ad9b1032d0886e1f1d3ac95942baa4e
YukiWatanabe824 <gyggg950@gmail.com>
2022-12-25 18:16:07 +0900

    refactor: moment.jsをdayjsに変更 / 日付取得の条件分岐を修正 / 出力時の改行or空白の代入方法を変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,14 +1,14 @@
-const moment = require("moment");
+//const moment = require("moment");
+const dayjs = require('dayjs');
+const weekday = require('dayjs/plugin/weekday')
+dayjs.extend(weekday)
+
 const argv = require("minimist")(process.argv.slice(2));
 
 function get_date(argv) {
-  let year = Object.prototype.hasOwnProperty.call(argv, "y")
-    ? argv.y
-    : moment().year();
-  let month = Object.prototype.hasOwnProperty.call(argv, "m")
-    ? argv.m - 1
-    : moment().month();
-  return moment([year, month, 1]);
+  const year = argv.y ?? dayjs().year();
+  const month = argv.m ?? (dayjs().month() + 1);
+  return dayjs([year, month, 1]);
 }
 
 function generate_full_days(date) {
@@ -38,9 +38,9 @@ function generate_calendar(date, full_days) {
   console.log("日 月 火 水 木 金 土");
   for (let i = 0; i < full_days.length; i++) {
     if ((i + 1) % 7 === 0) {
-      process.stdout.write(full_days[i] + "\n");
+      process.stdout.write(full_days[i].padEnd(3, "\n"));
     } else {
-      process.stdout.write(full_days[i] + " ");
+      process.stdout.write(full_days[i].padEnd(3, " "));
     }
   }
   console.log("");
